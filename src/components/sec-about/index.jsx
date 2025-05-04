1ac0ceaaada0a16ea6a8b7e5f231332521d4789a
Olatunji Lawal <lawaltreasure08@gmail.com>
2025-05-04 17:09:31 +0100

    cards functionality

--- src/components/sec-about/index.jsx
+++ src/components/sec-about/index.jsx
@@ -1,12 +1,45 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function AboutTwo() {
+
+  const [arr, setArr] = useState([
+    {
+      h3_txt: "after-sale <span>support</span> <br /> and <span>maintenance</span>",
+      p_txt: "Our commitment to your satisfaction extends beyond the trial installation. we conduct a thorough trial walkthrough to ensure your satisfaction and also comprehensive after-sales for majority claims, manufacture, and cane instructions."
+    },
+    {
+      h3_txt: "after-sale <span>support</span> <br /> and <span>maintenance</span>",
+      p_txt: "Our commitment to your satisfaction extends beyond the trial installation. we conduct a thorough trial walkthrough to ensure your satisfaction and also comprehensive after-sales for majority claims, manufacture, and cane instructions."
+    },
+    {
+      h3_txt: "after-sale <span>support</span> <br /> and <span>maintenance</span>",
+      p_txt: "Our commitment to your satisfaction extends beyond the trial installation. we conduct a thorough trial walkthrough to ensure your satisfaction and also comprehensive after-sales for majority claims, manufacture, and cane instructions."
+    },
+    {
+      h3_txt: "after-sale <span>support</span> <br /> and <span>maintenance</span>",
+      p_txt: "Our commitment to your satisfaction extends beyond the trial installation. we conduct a thorough trial walkthrough to ensure your satisfaction and also comprehensive after-sales for majority claims, manufacture, and cane instructions."
+    },
+    {
+      h3_txt: "after-sale <span>support</span> <br /> and <span>maintenance</span>",
+      p_txt: "Our commitment to your satisfaction extends beyond the trial installation. we conduct a thorough trial walkthrough to ensure your satisfaction and also comprehensive after-sales for majority claims, manufacture, and cane instructions."
+    }
+  ])
+
+  function showCard(card_Index) {
+    const dummy = [...arr];
+    const data = dummy.splice(card_Index, 1)[0]; // get the object directly
+    dummy.unshift(data); // insert it at the beginning
+    setArr(dummy);
+  }
+  
+  
   return (
     <div className="about-2">
       <div className="txt-box">
         <h2>how we <span>simplify</span> your <br /> furnishing experience</h2>
       </div>
-      <div className="about-2-container-1">
+      {/* <div className="about-2-container-1">
         <div className="card">
           <div className="left-col"></div>
           <div className="img"></div>
@@ -26,13 +59,28 @@ export default function AboutTwo() {
         <div className="card">
           <div className="left-col">
             <h2>5</h2>
-            <h3>after-sale <span>support</span> <br /> and <span>maintenance</span></h3>
-            <p>
-              Our commitment to your satisfaction extends beyond the trial installation. we conduct a thorough trial walkthrough to ensure your satisfaction and also comprehensive after-sales for majority claims, manufacture, and cane instructions.
-            </p>
+            <h3></h3>
+            <p></p>
           </div>
           <div className="img"></div>
         </div>
+      </div> */}
+
+      <div className="about-2-container-1">
+        {
+          arr.map((item, i) => {
+            return (
+              <div className="card" onClick={() => showCard(i)} key={i}>
+                <div className="left-col">
+                  <h2>{i+1}</h2>
+                  <h3 dangerouslySetInnerHTML={{ __html: item.h3_txt }}></h3>
+                  <p>{item.p_txt}</p>
+                </div>
+                <div className="img"></div>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   );
